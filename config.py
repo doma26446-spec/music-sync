a1f5b345a4444fed0e5ce0f7d7956cbe327bd33d
doma26446-spec <doma26446@gmail.com>
2026-07-08 00:51:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8822962487:AAElJnW-nIstzcO_ocjFIL1agO4It8rcmKQ")
-WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://4ab6a54c5ac414.lhr.life")
+WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://music-sync-rby2.onrender.com")
 HOST = "0.0.0.0"
 PORT = int(os.environ.get("PORT", 8000))
